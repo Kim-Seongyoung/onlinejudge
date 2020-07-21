2de0db8df181e84f8914d86b2cc3ccba47439f1b
Kim-Seongyoung <oeoe951@gmail.com>
2020-07-21 23:33:35 +0900

    20200721 풀이

---
+++ programmers/기지국설치.java
@@ -0,0 +1,35 @@
+package programmers;
+
+public class 기지국설치 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int solution(int n, int[] stations, int w) {
+	       int answer = 0;
+			int l = 2*w+1;
+			int curr = 1;
+			int pivot = 0;
+			while (curr <= n) {
+				if (pivot < stations.length) {
+					if (curr < stations[pivot] - w) {
+						answer++;
+						curr += l;
+					} else {
+						curr = stations[pivot]+w + 1;
+						pivot++;
+					}
+				}else {
+					int temp = (n-curr)+1;
+					int result = temp/l;
+					if(temp%l!=0) {
+						result++;
+					}
+					answer += result;
+					break;
+				}
+			}
+	        return answer;
+	    }
+}
